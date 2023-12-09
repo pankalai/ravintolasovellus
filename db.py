101922142c5fb184b363c10481a0f3b0e9b6e31b
Anssi Pänkäläinen <anssi.pankalainen@gmail.com>
2023-12-09 19:06:31 +0200

    refactor categories

--- db.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
-from app import app
-
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-
-db = SQLAlchemy(app)

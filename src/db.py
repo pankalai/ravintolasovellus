b1c8bbec6075f197dfc1fcc3f005bcb9c56ca219
Anssi Pänkäläinen <anssi.pankalainen@gmail.com>
2023-12-15 15:06:29 +0200

    pylint fixes

--- src/db.py
+++ src/db.py
@@ -1,10 +1,22 @@
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
-from sqlalchemy import exc
+from sqlalchemy.sql import text, exc
 from app import app
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
 db = SQLAlchemy(app)
 
+# class Database:
+#     def execute_command(self):
+#         try:
+#             pass
+#         except Exception as error:
+#             print("Tapahtui virhe: ", error)
+#             database.session.rollback()
+#             return False
+#         return True
+
+
+
+# db = Database()
